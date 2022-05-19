381058501eb6680ed2677c5a9ef15864284c9598
touraye <touraye07@gmail.com>
2022-05-19 10:01:32 +0000

    setup store

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
+import cartItems from '../../cartItems'
 
 const initialState = {
-  cartItems: [],
-  amount: 0,
-  total: 0,
-  isLoading: true
+	cartItems: cartItems,
+	amount: 2,
+	total: 0,
+	isLoading: true,
 }
 
 const cartSlice = createSlice( {
