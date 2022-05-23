a4ef00f6f45f6f5307ad2333faaa5194d3879ef3
touraye <touraye07@gmail.com>
2022-05-23 00:47:32 +0000

    creating reducers

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+
+import { calculateTotals } from './features/cart/cartSlice'
+
 function App() {
+  const {cartItems} = useSelector((store)=> store.cart)
+  const dispatch = useDispatch()
+
+  useEffect( () => { 
+    dispatch(calculateTotals())
+  }, [cartItems] )
+  
   return (
     <main>
       <Navbar />
